7b3a16d44a522ac4f27cc1bfafba5e3507d770cf
Joshua D. Arnow <1476967+josharnow@users.noreply.github.com>
2022-03-23 10:41:22 -0400

    Completed login & registration functionality

---
+++ flask_app/controllers/login.py
@@ -0,0 +1,83 @@
+from flask import render_template, redirect, session, request, flash
+from flask_app import app
+
+from flask_app.models.user import User
+
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+@app.route('/')
+def index():
+  """Renders the login/registration page
+
+  Returns:
+      str: The result of evaluating index.html
+  """  
+  
+  return render_template('index.html')
+
+
+@app.route('/users/register', methods=['POST'])
+def register_user():
+  """Registers user
+
+  Returns:
+      requests.Response: The response dictionary (object)
+  """  
+  
+  # Validate the form data
+  if User.validate_registration(request.form):
+    # Hash password here
+    data = {
+      'username': request.form['username'],
+      'email': request.form['email'],
+      'password': bcrypt.generate_password_hash(request.form['password'])
+    }
+    
+    user.id = User.create_user(data)
+    
+    session['user_id'] = user.id
+    
+    return redirect('/success')
+    
+  # Create user if data is valid
+  return redirect('/')
+
+@app.route('/users/login', methods=['POST'])
+def login_user():
+  """Logs user in
+
+  Returns:
+      requests.Response: The response object
+  """  
+  users = User.get_users_with_username(request.form)
+  
+  # if len(users) == 0: # Can use this instead, too
+  if len(users) != 1: # If there are (mistakenly) two users with the same nickname in the database, this conditional will prevent a login to the first user that it finds with the given username [can use other conditional here considering that the relevant valedations are performed when a user is registered]
+    flash('User with the given username does not exist.')
+    return redirect('/')
+  
+  user = users[0]
+  
+  if not bcrypt.check_password_hash(user.password, request.form['password']):
+    flash('Password for the given user is incorrect.')
+    return redirect('/')
+  
+  # flash('Login successful')
+  session['user_id'] = user.id
+  session['user_username'] = user.username
+  
+  return redirect('/success')
+
+@app.route('/success')
+def success():
+  if 'user_id' not in session:
+    flash('Please log in to view this page.')
+    return redirect('/')
+  
+  return render_template('success.html')
+
+@app.route('/logout')
+def logout():
+  session.clear()
+  return redirect('/')
---
+++ server.py
@@ -0,0 +1,6 @@
+from flask_app import app
+from flask_app.controllers import login
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
+    # app.run(debug=True) should be the very last statement! 
