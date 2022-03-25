dfa0c0564e751c5ed2626160b85a448ed33a7ef3
Joshua D. Arnow <1476967+josharnow@users.noreply.github.com>
2022-03-25 18:53:12 -0400

    Edited server.py

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 from flask_app import app
 from flask_app.controllers import login, companies
-from waitress import serve
+# from waitress import serve
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-    serve(app, host="0.0.0.0", port=5000)
+    # serve(app, host="0.0.0.0", port=5000)
+    app.run(debug=False)    # Run the app in debug mode.
     # app.run(debug=True)    # Run the app in debug mode.
     # app.run(debug=True) should be the very last statement! 
