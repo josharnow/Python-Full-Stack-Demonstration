1d0ddccd87dcd3eefc2df034b41885df58141edc
Joshua D. Arnow <1476967+josharnow@users.noreply.github.com>
2022-03-28 16:46:54 -0400

    Updated requirements.txt

--- server.py
+++ server.py
@@ -4,6 +4,5 @@ from flask_app.controllers import login, companies
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     # serve(app, host="0.0.0.0", port=5000)
-    app.run(debug=False)    # Run the app in debug mode.
-    # app.run(debug=True)    # Run the app in debug mode.
+    app.run(debug=True)    # Run the app in debug mode.
     # app.run(debug=True) should be the very last statement! 
